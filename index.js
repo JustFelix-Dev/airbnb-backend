1fbee9f731bf851157d3776d85c51ebadabe6a5f
Felix Owolabi <owolabifelix78@gmail.com>
2023-08-28 16:40:07 +0100

    server

--- index.js
+++ index.js
@@ -32,12 +32,11 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(express.static('public'));
 app.use('/uploads',express.static(__dirname+'/uploads'))
-app.use('/userPhoto',express.static(__dirname+'/userPhoto'))
 app.set('view engine','ejs')
 app.use(express.urlencoded({extended: false}))
 app.use(cors({
     credentials: true,
-       origin:['https://www.airbnb.felixdev.com.ng','http:localhost:5174'],
+       origin:'https://www.airbnb.felixdev.com.ng',
     }))
 
 // Social Auth
