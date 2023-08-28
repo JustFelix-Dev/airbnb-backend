bf01e36271fce883b153692410f27ed023a561b3
Felix Owolabi <owolabifelix78@gmail.com>
2023-08-28 14:25:06 +0100

    server

--- uploadImages.js
+++ uploadImages.js
@@ -5,7 +5,7 @@ configDotenv();
 cloudinary.config({ 
   cloud_name: process.env.CLOUD_NAME, 
   api_key: process.env.CLOUD_API, 
-  api_secret: process.env.CLOUD_SECRET 
+  api_secret: process.env.CLOUD_SECRET,
 });
 
 module.exports = cloudinary;
