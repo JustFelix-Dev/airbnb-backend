06a22dae68b1f82e5d2728184d25dc23954e4e5f
Felix Owolabi <owolabifelix78@gmail.com>
2023-09-04 14:41:18 +0100

    airbnb

--- index.js
+++ index.js
@@ -32,10 +32,11 @@ require('https').globalAgent.options.rejectUnauthorized = false;
 
 
 // Middleware
+app.use(express.static('public'));
 app.use(cookieParser())
 app.use(express.json())
-app.use(express.static('public'));
 app.set('view engine','ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended: false}))
 app.use(express.json({
     limit: '50mb'
